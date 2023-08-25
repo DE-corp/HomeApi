cec749120af5d00a79766ccecf8ffb23e3784f91
DE-corp <crazyHOPHOP@rambler.ru>
2023-08-25 20:28:03 +0300

    Поправил Configuration

--- HomeApi/Startup.cs
+++ HomeApi/Startup.cs
@@ -1,17 +1,10 @@
 using HomeApi.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HomeApi
 {
@@ -19,6 +12,8 @@ namespace HomeApi
     {
         private IConfiguration Configuration
         { get; } = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile("appsettings.Development.json")
           .AddJsonFile("HomeOptions.json")
           .Build();
 
